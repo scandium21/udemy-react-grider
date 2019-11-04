7c888164444b19086df246c41c1f409a53c4e8e0
Siliang Chang <siliang.chang@gmail.com>
2019-11-03 20:37:04 -0700

    react state

---
+++ seasons/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
